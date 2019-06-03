7ca750ea8818419fdaa551df05f8171de9ce41ad
deyvisgc <deivisgarcia@upeu.edu.pe>
2019-06-03 01:20:17 -0500

    hola

--- resources/views/Planilla/boleta.blade.php
+++ resources/views/Planilla/boleta.blade.php
@@ -65,6 +65,9 @@
                         <td>sasasa</td>
                         <td>sasasasa</td>
                         <td>sdsdsdsd</td>
+                        <td>sasasa</td>
+                        <td>sasasasa</td>
+                        <td>sdsdsdsd</td>
                         </tbody>
                     </table>
                 </div>
@@ -76,6 +79,93 @@
 
             </section>
         </div><br>
+        <div class="row">
+            <section class="co col-12">
+                <div class="table-responsive" >
+                    <table id="tb_total2" style="background-color:#F7BFBF " class="table table-striped table-bordered "  width="100%">
+                        <thead>
+                        <tr>
+                            <th  style="background-color:#E58484 "><i class="fa fa-fw fa-dollar text-muted hidden-md hidden-sm hidden-xs"></i>CARGO</th>
+                            <th style="background-color:#E58484 "><i class="fa fa-fw fa-dollar text-muted hidden-md hidden-sm hidden-xs"></i>PERIODIC.</th>
+                            <th style="background-color:#E58484 "><i class="fa fa-fw fa-dollartext-muted hidden-md hidden-sm hidden-xs"></i>ONP</th>
+                            <th style="background-color:#E58484 "><i class="fa fa-fw fa-dollartext-muted hidden-md hidden-sm hidden-xs"></i>A.F.P.</th>
+                            <th style="background-color:#E58484 "><i class="fa fa-fw fa-dollar text-muted hidden-md hidden-sm hidden-xs"></i>C.U.S.P.P.</th>
+                            <th style="background-color:#E58484 "><i class="fa fa-fw fa-dollartext-muted hidden-md hidden-sm hidden-xs"></i>F. ING.</th>
+                            <th style="background-color:#E58484 "><i class="fa fa-fw fa-dollartext-muted hidden-md hidden-sm hidden-xs"></i>F. CESE</th>
+                            <th style="background-color:#E58484 "><i class="fa fa-fw fa-dollar text-muted hidden-md hidden-sm hidden-xs"></i>INI. VAC.</th>
+                            <th style="background-color:#E58484 "><i class="fa fa-fw fa-dollartext-muted hidden-md hidden-sm hidden-xs"></i>FIN VAC.</th>
+                            <th style="background-color:#E58484 "><i class="fa fa-fw fa-dollartext-muted hidden-md hidden-sm hidden-xs"></i>DIAS VAC</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <td>sdsdsds</td>
+                        <td>sasasa</td>
+                        <td>sasasasa</td>
+                        <td>sdsdsdsd</td>
+                        <td>sasasa</td>
+                        <td>sasasasa</td>
+                        <td>sdsdsdsd</td>
+                        <td>sasasa</td>
+                        <td>sasasasa</td>
+                        <td>sdsdsdsd</td>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+
+        </div>
+        <div class="row">
+            <section class="co col-6">
+                <div class="table-responsive" >
+                    <table id="tb_total2" style="background-color:#F7BFBF " class="table table-striped table-bordered "  width="100%">
+                        <thead>
+                        <tr>
+                            <th  style="background-color:#E58484 ">DIAS LABORADOS</th>
+                            <th style="background-color:#E58484 ">TOTAL HORAS LABORADAS</th>
+                            <th style="background-color:#E58484 ">HORAS EXTRAS AL 25%</th>
+                            <th style="background-color:#E58484 ">HORAS EXTRAS AL 35%</th>
+                            <th style="background-color:#E58484 ">DIAS NO LABORADOS</th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <td>sdsdsds</td>
+                        <td>sasasa</td>
+                        <td>sasasasa</td>
+                        <td>sdsdsdsd</td>
+                        <td>sasasa</td>
+
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+            <section class="co col-3">
+                <div class="table-responsive" >
+                    <table id="tb_total2" style="background-color:#F7BFBF " class="table table-striped table-bordered "  width="100%">
+                        <tr>
+                            <td style="background-color:#E58484" rowspan="3">OTRO EMPLEADOR</td><td>IMPORTE REMUN.</td>
+                        </tr>
+                        <tr>
+                            <td>0.00</td>
+                        </tr>
+                    </table>
+                </div>
+            </section>
+            <section class="co col-3">
+                <div class="table-responsive" >
+                    <table id="tb_total2" style="background-color:#F7BFBF " class="table table-striped table-bordered "  width="100%">
+                        <thead>
+                        <tr>
+                            <th  style="background-color:#E58484 ">CTA CTE DE DEPÓSITO</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <td style="padding-top: 40px;">sdsdsds</td>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        </div>
     </div>
 
     @endsection
